8070385d7a80dc454184e4227478bd351da4b5ac
Younes Oubellal <younesoubllal@gmail.com>
2026-01-13 23:56:45 +0100

    p

--- src/node.ts
+++ src/node.ts
@@ -1,15 +1,28 @@
 import express, {Request, Response} from 'express';
 
 const	PORT = process.env.PORT || 5000;
+const	KEY: string = "1337";
 const	app = express();
 app.get("/api", (req: Request, res: Response) =>
 {
-	res.json(
+	const	key = req.query.apiKey;
+	if (key === KEY)
 	{
-		name: "youbella",
-		age: 19,
-		scholl: "1337",
-	});
+		res.json(
+		{
+			name: "youbella",
+			age: 19,
+			scholl: "1337",
+		});
+	}
+	else
+	{
+		res.status(401);
+		res.json(
+		{
+			error: "wrong APi KEY",
+		});
+	}
 });
 
 app.listen(PORT, () => (console.log(`Is listen in port ${PORT}`)))
