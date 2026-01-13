ee195fcdb5d314c719e71d596cb0447fe52a2ec1
Younes Oubellal <younesoubllal@gmail.com>
2026-01-13 23:38:17 +0100

    del

--- frontend/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() 
-{
-	fetch("http://localhost:4000/api").then(response => (response.json())).then(response => (console.log(response))).catch(err => (console.log("Error in fetch data api " + err)));
-	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-		</div>
-	);
-}
