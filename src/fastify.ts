a74276fc31fcb57c325dafeb40cf86b6d877ba06
Younes Oubellal <younesoubllal@gmail.com>
2026-01-17 15:00:28 +0100

    push

---
+++ src/fastify.ts
@@ -0,0 +1,84 @@
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import { products } from "./products";
+
+const	server = fastify({logger: true});
+server.register(cors,
+{
+	origin: "http://localhost:3002",
+});
+
+interface	params
+{
+	id: string;
+}
+
+const	schema =
+{
+	schema:
+	{
+		response:
+		{
+			200:
+			{
+				type: "array",
+				items:
+				{
+					type: "object",
+					required: ["id", "title", "price", "old_price", "image", "gategory", "is_new_product"],
+					properties:
+					{
+						id: {type: "number"},
+						title: {type: "string"},
+						price: {type: "number"},
+						old_price: {type: ["number", "null"], default: null},
+						image: {type: "string"},
+						gategory: {type: "string"},
+						is_new_product: {type: "boolean"},
+					}
+				}
+			},
+			400:
+			{
+				type: "object",
+				items:
+				{
+					required: ["error"],
+					properties:
+					{
+						error: {type: "string"},
+					}
+				}
+			},
+		}
+	}
+}
+
+server.get("/", (req, res) =>
+{
+	res.send({name: "youbella"});
+});
+
+server.get<{Params: params}>("/products", schema, async (req, res) =>
+{
+	res.send(products);
+});
+server.get<{Params: params}>("/products/:id", schema, async (req, res) =>
+{
+	let	{id} = req.params;
+	let	product = products.filter((value) => (value.id === Number(id)));
+	if (product.length)
+		res.send(product);
+	else
+		res.status(404).send({error: "InValid id product"});
+});
+
+server.listen({port: Number(process.env.PORT) || 3001}, (err, address) =>
+{
+	if (err)
+	{
+		console.log(err);
+		process.exit(1);
+	}
+	console.log(address);
+});
---
+++ src/products.ts
@@ -0,0 +1,91 @@
+export let	products =
+[
+	{
+		"id": 1,
+		"title": "32\" Smart TV HD",
+		"price": 1302.3,
+		"old_price": 1802.9,
+		"image": "/products_images/p1.jpg",
+		"gategory": "electorinic",
+		"is_new_product": true
+	},
+	{
+		"id": 2,
+		"title": "Laptop Gaming",
+		"price": 8500,
+		"image": "/products_images/p2.jpg",
+		"gategory": "electorinic",
+		"is_new_product": false
+	},
+	{
+		"id": 3,
+		"title": "MacBook Pro",
+		"price": 1220,
+		"old_price": 1337,
+		"image": "/products_images/p3.jpg",
+		"gategory": "electorinic",
+		"is_new_product": true
+	},
+	{
+		"id": 4,
+		"title": "MakeUp",
+		"price": 15.99,
+		"old_price": 20.99,
+		"image": "/products_images/p3.jpg",
+		"gategory": "beuaty",
+		"is_new_product": true
+	},
+	{
+		"id": 5,
+		"title": "FootBall",
+		"price": 125.5,
+		"image": "/products_images/p4.jpg",
+		"gategory": "sport",
+		"is_new_product": false
+	},
+	{
+		"id": 6,
+		"title": "Basketball",
+		"price": 150,
+		"old_price": 200,
+		"image": "/products_images/p3.jpg",
+		"gategory": "sport",
+		"is_new_product": true
+	},
+	{
+		"id": 7,
+		"title": "Face primer",
+		"price": 5,
+		"old_price": 8,
+		"image": "/products_images/p5.jpg",
+		"gategory": "beuaty",
+		"is_new_product": true
+	},
+	{
+		"id": 8,
+		"title": "Sabon",
+		"price": 5,
+		"old_price": 8,
+		"image": "/products_images/p5.jpg",
+		"gategory": "beuaty",
+		"is_new_product": true
+	},
+	{
+		"id": 9,
+		"title": "Shoes Sport X",
+		"price": 150,
+		"old_price": 200,
+		"image": "/products_images/p3.jpg",
+		"gategory": "sport",
+		"is_new_product": true
+	},
+	{
+		"id": 10,
+		"title": "DELL 32Ram 1Tr SSD ddr5",
+		"price": 1220,
+		"old_price": 1337,
+		"image": "/products_images/p3.jpg",
+		"gategory": "electorinic",
+		"is_new_product": true
+	}
+];
